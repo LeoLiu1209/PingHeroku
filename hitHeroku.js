853ce95f98e2bfb82fabe1c930e0bd8a20960602
leoliu <cubemail88@gmail.com>
2017-07-10 21:16:40 +0800

    first release

---
+++ hitHeroku.js
@@ -0,0 +1,31 @@
+var moment = require('moment')
+var request =require('request')
+const url='' //Heroku url
+const intervalTime=30*60*1000 //ping every 30 mins
+
+setInterval(() => {
+    getTime((InStopPingTime, currentTime) => {
+        if(!InStopPingTime){
+        	reqHeroku(currentTime)
+        	return
+        }
+    	console.log(currentTime+' is not in ping time')
+    })
+  }, intervalTime);
+
+function getTime(callback){
+	let currentTime = moment()
+	let extra = moment().format('YYYY-MM-DD') + ' '
+	let stop_time_from = moment(extra + '00:30')
+	let stop_time_end = moment(extra + '08:30')
+    let InStopPingTime=moment(currentTime).isBetween(stop_time_from,stop_time_end)
+
+	currentTime=currentTime.format('hh:mm:ss a')
+    callback(InStopPingTime,currentTime)
+}
+
+function reqHeroku(currentTime){
+  	request(url, ()=>{
+  		console.log(currentTime + ' Heroku-self-ping: ' + url)
+  	})
+}
