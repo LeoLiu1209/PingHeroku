d120d884071f6e66285125b344715e470714f18a
LeoLiu1209 <cubemail88@gmail.com>
2017-07-10 21:22:45 +0800

    fix js formate

--- hitHeroku.js
+++ hitHeroku.js
@@ -1,27 +1,26 @@
 var moment = require('moment')
 var request =require('request')
-const url='' //Heroku url
+const url='' 		       //Heroku url
 const intervalTime=30*60*1000 //ping every 30 mins
 
 setInterval(() => {
     getTime((InStopPingTime, currentTime) => {
         if(!InStopPingTime){
-        	reqHeroku(currentTime)
+		reqHeroku(currentTime)
         	return
         }
-    	console.log(currentTime+' is not in ping time')
+    console.log(currentTime+' is not in ping time')
     })
-  }, intervalTime);
+  }, intervalTime)
 
 function getTime(callback){
 	let currentTime = moment()
 	let extra = moment().format('YYYY-MM-DD') + ' '
 	let stop_time_from = moment(extra + '00:30')
 	let stop_time_end = moment(extra + '08:30')
-    let InStopPingTime=moment(currentTime).isBetween(stop_time_from,stop_time_end)
-
+    	let InStopPingTime=moment(currentTime).isBetween(stop_time_from,stop_time_end)
 	currentTime=currentTime.format('hh:mm:ss a')
-    callback(InStopPingTime,currentTime)
+    	callback(InStopPingTime,currentTime)
 }
 
 function reqHeroku(currentTime){
