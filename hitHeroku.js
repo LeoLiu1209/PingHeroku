db6fc8f597658fd7dd63b4da9ff9ad00ab21ffff
LeoLiu1209 <cubemail88@gmail.com>
2017-07-14 09:55:13 +0800

    set up for default interval ping time

--- hitHeroku.js
+++ hitHeroku.js
@@ -1,7 +1,7 @@
 var moment = require('moment')
 var request =require('request')
 const url='' 		       //Heroku url
-const intervalTime=1*1000 //ping every 30 mins
+const intervalTime=25*60*1000 //ping every 25 mins
 
 setInterval(() => {
     getTime((InStopPingTime, currentTime) => {
@@ -18,9 +18,9 @@ function getTime(callback){
 	let extra = moment().format('YYYY-MM-DD') + ' '
 	let stop_time_from = moment(extra + '00:30')
 	let stop_time_end = moment(extra + '08:30')
-    let InStopPingTime=moment(currentTime).isBetween(stop_time_from,stop_time_end)
+    	let InStopPingTime=moment(currentTime).isBetween(stop_time_from,stop_time_end)
 	currentTime=currentTime.format('hh:mm:ss a')
-    callback(InStopPingTime,currentTime)
+    	callback(InStopPingTime,currentTime)
 }
 
 function reqHeroku(currentTime){
